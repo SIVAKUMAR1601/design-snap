42edf1a9d993a6d0e2166deef876ce02b336bbc8
Sivakumar <sivask2122kdr@gmail.com>
2023-07-29 12:11:33 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,42 @@
+// $(document).ready(function () {
+   
+//     $('.mobile_menu').hide();
+// $('#btn_open').click(function (e) { 
+//     $('.mobile_menu').toggle();
+    
+// });
+// $('#btn_close').click(function (e) { 
+//     $('.mobile_menu').toggle();
+    
+// });
+    
+$(document).ready(function () {
+    // Hide the mobile menu initially
+    $('.mobile_menu').hide();
+  
+    $('#btn_open').click(function () {
+      // Show the mobile menu
+      $('.mobile_menu').show();
+    });
+  
+    $('#btn_close').click(function () {
+      // Hide the mobile menu
+      $('.mobile_menu').hide();
+    });
+  });
+  
+    
+    // $('.mobile_menu').hide();
+    
+
+    // $('#btn_open').click(function () {
+    //     $('.mobile_menu').show();
+
+    // });
+    // $("#btn_close").click(function () {
+    //     $('.mobile_menu').hide();
+
+    // });
+
+    // $('.mobile_menu').hide();
+// });
