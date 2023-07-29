d92a0589e0e7dc16c5cd8e7e7094a58d1ef4f6ac
Sivakumar <sivask2122kdr@gmail.com>
2023-07-29 12:31:09 +0530

    fixed mobile menu

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-// $(document).ready(function () {
-   
-//     $('.mobile_menu').hide();
-// $('#btn_open').click(function (e) { 
-//     $('.mobile_menu').toggle();
-    
-// });
-// $('#btn_close').click(function (e) { 
-//     $('.mobile_menu').toggle();
-    
-// });
-    
 $(document).ready(function () {
     // Hide the mobile menu initially
     $('.mobile_menu').hide();
@@ -25,18 +13,3 @@ $(document).ready(function () {
     });
   });
   
-    
-    // $('.mobile_menu').hide();
-    
-
-    // $('#btn_open').click(function () {
-    //     $('.mobile_menu').show();
-
-    // });
-    // $("#btn_close").click(function () {
-    //     $('.mobile_menu').hide();
-
-    // });
-
-    // $('.mobile_menu').hide();
-// });
